3f722292977a46dd13b64b916628091c7f33d739
Thang <duythangdoan1994@gmail.com>
2018-06-13 10:21:25 +0700

    add setting database

--- egoo_core/settings.py
+++ egoo_core/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'egoo_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'egoo-core',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
