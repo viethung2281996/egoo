7daf69970b1cae1656f0283afff2737625e2a20a
Thang <duythangdoan1994@gmail.com>
2018-06-13 10:34:04 +0700

    add authentication api app

--- egoo_core/settings.py
+++ egoo_core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'egoo_core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'egoo-core',
+        'NAME': 'egoo_core',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
