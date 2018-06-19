f5f391db6fca3c502b5583cf6b5a712b0676ad9a
Thang <duythangdoan1994@gmail.com>
2018-06-19 19:47:13 +0700

    add register

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import path, include
+from user.views import CreateUserView
 
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
-    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('rest-auth/registration/', CreateUserView.as_view(), name='user_register'),
 ]
