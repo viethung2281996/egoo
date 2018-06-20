bf83a276e7406f77b63729909b276185fc72c1ba
Viet Hung <viethung2281996@gmail.com>
2018-06-21 00:07:51 +0700

    update units api

--- egoo_core/settings.py
+++ egoo_core/settings.py
@@ -88,12 +88,7 @@ AUTHENTICATION_BACKENDS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
 }
 
--- units/serializers.py
+++ units/serializers.py
@@ -1,14 +1,32 @@
 from units.models import Unit
+from django.db import models
+from categories.models import Category
 from rest_framework import serializers
 from categories.serializers import CategorySerializer
 
-class UnitSerializer(serializers.ModelSerializer):
+class UnitSerializer(serializers.HyperlinkedModelSerializer):
   category = CategorySerializer(read_only=True)
+  category_id = models.IntegerField(null=False)
+
+  def create(self, validated_data):
+    import pdb; pdb.set_trace()
+    category = Category.objects.get(pk=validated_data["category_id"])
+    unit = Unit.objects.create(
+        title = validated_data["title"],
+        order = validated_data["order"],
+        category = category
+      )
+   
+    if unit.save():
+      return unit
+
   class Meta:
     fields = (
       'id',
       'title',
       'order',
-      'category'
+      'category',
+      'category_id'
       )
     model = Unit
+    depth=1
