3f08af27391e017c01baa6b0a6179b3bb623bc11
Viet Hung <viethung2281996@gmail.com>
2018-06-21 17:22:21 +0700

    remove url.py in categories conversations units

--- api/urls.py
+++ api/urls.py
@@ -7,10 +7,13 @@ from rest_framework_jwt.views import refresh_jwt_token
 
 
 urlpatterns = [
+    #authenticate url
     path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', CreateUserView.as_view(), name='user_register'),
+    path('refresh-token/', refresh_jwt_token),
+
+    #app url
     path('categories/', include('categories.urls')),
     path('units/', include('units.urls')),
     path('conversations/', include('conversations.urls')),
-    path('rest-auth/registration/', CreateUserView.as_view(), name='user_register'),
-    path('refresh-token/', refresh_jwt_token),
 ]
