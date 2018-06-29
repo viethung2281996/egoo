8d3a322992a1996ab5ed9b351029792ce0de31d8
Viet Hung <viethung2281996@gmail.com>
2018-06-29 09:59:13 +0700

    update upload file and image

--- notes/serializers.py
+++ notes/serializers.py
@@ -4,7 +4,7 @@ from units.models import Unit
 
 class NoteSerializer(serializers.ModelSerializer):
   units = serializers.PrimaryKeyRelatedField(many=True, queryset=Unit.objects.all())
-  audio = serializers.FileField(max_length=None, use_url=True, allow_empty_file=True, read_only=True)
+  audio = serializers.FileField(max_length=None, use_url=True, allow_empty_file=True, required=False)
 
   class Meta:
     fields = (
