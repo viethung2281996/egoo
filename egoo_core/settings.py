7ce0353e9ea961ff7ca0ac500291ff872793c3d9
Viet Hung <viethung2281996@gmail.com>
2018-07-05 10:22:49 +0700

    update setting

--- egoo_core/settings.py
+++ egoo_core/settings.py
@@ -14,6 +14,8 @@ import os
 import datetime
 import socket
 import cloudinary
+import dj_database_url
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -138,6 +140,22 @@ else:
     from egoo_core.config_server import *
 # Database 
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'egoo_core',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
+
+django_heroku.settings(locals())
+db_from_env=dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
