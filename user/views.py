22c9c61c5409a98919f1b3b8e2d58cef79af0882
Viet Hung <viethung2281996@gmail.com>
2018-10-05 00:07:25 +0700

    Fix bug when CustomInformation does not exist

--- user/views.py
+++ user/views.py
@@ -38,14 +38,15 @@ class UserUploadAvatar(APIView):
     try:
       user = get_user_model().objects.get(id=pk)
       custom_information = user.custominformation
-      if custom_information is None :
-        custom_information = CustomInformation(user=user)
-        custom_information.save()
     except ObjectDoesNotExist:
       response = {
          "message": "Object doesn't exist"
       }
       return Response(response)
+    except CustomInformation.DoesNotExist:
+      custom_information = CustomInformation(user=user)
+      custom_information.save()
+
     data = {}
     file = request.data['image']
     file_name = custom_information.init_file_name(file)
