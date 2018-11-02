38adf62bfaf7170b9fc68e2edcc5872b9b8511fe
Viet Hung <viethung2281996@gmail.com>
2018-11-02 23:51:14 +0700

    update config server

--- egoo_core/config_server.py
+++ egoo_core/config_server.py
@@ -1,4 +1,6 @@
 import cloudinary
+from egoo_core.settings import INSTALLED_APPS
+import os
 
 # Cloudinary settings for Django. Add to your settings file.
 cloudinary.config(
@@ -6,3 +8,17 @@ cloudinary.config(
   api_key = '968886895255698',  
   api_secret = 'RvrscTeNAfWtshM-ftDpclOFF5I',  
 )
+
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
+
+BROKER_URL = os.environ.get("CLOUDAMQP_URL", "django://")
+BROKER_POOL_LIMIT = 1
+BROKER_CONNECTION_MAX_RETRIES = None
+
+CELERY_TASK_SERIALIZER = "json"
+CELERY_ACCEPT_CONTENT = ["json", "msgpack"]
+
+if BROKER_URL == "django://":
+  INSTALLED_APPS += ("kombu.transport.django",)
