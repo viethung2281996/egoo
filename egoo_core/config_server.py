4549d9595725196d24b4e714c27fcc3710b2d6c1
Hungtv <viethung2281996@gmail.com>
2019-03-03 01:05:43 +0700

    Update config environment

--- egoo_core/config_server.py
+++ egoo_core/config_server.py
@@ -1,8 +1,24 @@
 import cloudinary
-
+import dj_database_url
+import django_heroku
 # Cloudinary settings for Django. Add to your settings file.
 cloudinary.config(
   cloud_name = 'egoodev',  
   api_key = '968886895255698',  
   api_secret = 'RvrscTeNAfWtshM-ftDpclOFF5I',  
 )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'egoo_core',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
+
+django_heroku.settings(locals())
+db_from_env=dj_database_url.config()
+DATABASES['default'].update(db_from_env)
